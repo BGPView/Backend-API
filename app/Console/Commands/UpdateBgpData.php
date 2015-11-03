723c89920d913b96f6db72a348a1e71788c9b011
Yif Swery <yiftachswr@gmail.com>
2015-11-03 23:11:11 +1300

    Finished up the BGP prefix importer/whois update

--- app/Console/Commands/UpdateBgpData.php
+++ app/Console/Commands/UpdateBgpData.php
@@ -3,10 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Helpers\IpUtils;
+use App\Models\IPv4PrefixEmail;
+use App\Models\IPv4Prefix;
 use App\Models\IPv6PrefixEmail;
 use App\Models\IPv6Prefix;
 use App\Services\BgpParser;
 use App\Services\Whois;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use League\CLImate\CLImate;
@@ -55,8 +58,8 @@ class UpdateBgpData extends Command
      */
     public function handle()
     {
-        $this->updateIPv6Prefixes();
         $this->updateIPv4Prefixes();
+        $this->updateIPv6Prefixes();
     }
 
     private function updateIPv6Prefixes()
@@ -85,10 +88,64 @@ class UpdateBgpData extends Command
                 // Skip of already in DB
                 $prefixTest = IPv6Prefix::where('ip', $parsedLine->ip)->where('cidr', $parsedLine->cidr)->first();
                 if (is_null($prefixTest) === false) {
-                    // ### TO DO: Now that we know the prefix is in DB we will need to update it or at least mark it
-                    // Also need to figure out a way to remove all stale prefixes
+
+                    $prefixTest->seen_at = Carbon::now();
+
+                    // If the last time the prefix was scraped is older than 7 days, update it
+                    if (strtotime($prefixTest->scraped_at) < Carbon::now()->subWeek()->timestamp) {
+                        $this->cli->br()->comment('===================================================');
+                        $this->cli->br()->comment('Updating older prefix whois info - ' . $prefixTest->ip . '/' . $prefixTest->cidr . ' [' . $ipAllocation->rir->name . ']')->br();
+
+                        $ipWhois = new Whois($prefixTest->ip);
+                        $parsedWhois = $ipWhois->parse();
+
+                        $prefixTest->name = $parsedWhois->name;
+                        $prefixTest->description = isset($parsedWhois->description[0]) ? $parsedWhois->description[0] : null;
+                        $prefixTest->description_full = json_encode($parsedWhois->description);
+                        $prefixTest->counrty_code = $parsedWhois->counrty_code;
+                        $prefixTest->owner_address = json_encode($parsedWhois->address);
+                        $prefixTest->raw_whois = $ipWhois->raw();
+                        $prefixTest->seen_at = Carbon::now();
+                        $prefixTest->scraped_at = Carbon::now();
+
+                        // Lets remove old emails for this prefix
+                        $prefixTest->emails()->delete();
+
+                        // Save new emails
+                        foreach ($parsedWhois->emails as $email) {
+                            $prefixEmail = new IPv6PrefixEmail;
+                            $prefixEmail->ipv6_prefix_id = $prefixTest->id;
+                            $prefixEmail->email_address = $email;
+
+                            // Check if its an abuse email
+                            if (in_array($email, $parsedWhois->abuse_emails)) {
+                                $prefixEmail->abuse_email = true;
+                            }
+
+                            $prefixEmail->save();
+                        }
+
+                        dump([
+                            'name' => $prefixTest->name,
+                            'description' => $prefixTest->description,
+                            'description_full' => json_decode($prefixTest->description_full, true),
+                            'counrty_code' => $prefixTest->counrty_code,
+                            'owner_address' => json_decode($prefixTest->owner_address, true),
+                            'abuse_emails' => $prefixTest->emails()->where('abuse_email', true)->get()->lists('email_address'),
+                            'emails' => $prefixTest->emails()->lists('email_address'),
+                        ]);
+
+                        unset($ipWhois);
+                        unset($parsedWhois);
+                    }
+
+                    // Update the prefix
+                    $prefixTest->save();
                     continue;
                 }
+
+                $this->cli->br()->comment('===================================================');
+                $this->cli->br()->comment('Adding new prefix whois info - ' . $parsedLine->ip . '/' . $parsedLine->cidr . ' [' . $ipAllocation->rir->name . ']')->br();
 
                 $ipWhois = new Whois($parsedLine->ip);
                 $parsedWhois = $ipWhois->parse();
@@ -105,9 +162,11 @@ class UpdateBgpData extends Command
                 $ipv6Prefix->counrty_code = $parsedWhois->counrty_code;
                 $ipv6Prefix->owner_address = json_encode($parsedWhois->address);
                 $ipv6Prefix->raw_whois = $ipWhois->raw();
+                $ipv6Prefix->seen_at = Carbon::now();
+                $ipv6Prefix->scraped_at = Carbon::now();
                 $ipv6Prefix->save();
 
-                // Save ASN Emails
+                // Save Prefix Emails
                 foreach ($parsedWhois->emails as $email) {
                     $prefixEmail = new IPv6PrefixEmail;
                     $prefixEmail->ipv6_prefix_id = $ipv6Prefix->id;
@@ -121,8 +180,22 @@ class UpdateBgpData extends Command
                     $prefixEmail->save();
                 }
 
+                dump([
+                    'name' => $ipv6Prefix->name,
+                    'description' => $ipv6Prefix->description,
+                    'description_full' => json_decode($ipv6Prefix->description_full, true),
+                    'counrty_code' => $ipv6Prefix->counrty_code,
+                    'owner_address' => json_decode($ipv6Prefix->owner_address, true),
+                    'abuse_emails' => $ipv6Prefix->emails()->where('abuse_email', true)->get()->lists('email_address'),
+                    'emails' => $ipv6Prefix->emails()->lists('email_address'),
+                ]);
+
+                // Remove all prefixes that are older than 1 day post udpating
+                IPv6Prefix::where('last_seen', '<', Carbon::yesterday())->delete();
             }
             fclose($fp);
+
+            //
         }
 
         $this->output->newLine(1);
@@ -139,19 +212,137 @@ class UpdateBgpData extends Command
     private function updateIPv4Prefixes()
     {
         $this->bench->start();
+        $this->progressStarted = false;
         $this->cli->br()->comment('===================================================');
         $filePath = sys_get_temp_dir() . '/ipv4_rib.txt';
 
         $this->downloadRIBs($filePath, 4);
 
+        $ipv4AmountCidrArray = $this->ipUtils->IPv4cidrIpCount();
+
         // Lets read through the file
         $fp = fopen($filePath, 'r');
         if ($fp) {
             while (($line = fgets($fp)) !== false) {
-                // ### TO DO: Here we will need to do the actual BGP entry processing
-                dump();
+                $parsedLine = $this->bgpParser->parse($line);
+                $ipAllocation = $this->ipUtils->getAllocationEntry($parsedLine->ip);
+
+                // Skip non allocated
+                if (is_null($ipAllocation) === true) {
+                    continue;
+                }
+
+                // Skip of already in DB
+                $prefixTest = IPv4Prefix::where('ip', $parsedLine->ip)->where('cidr', $parsedLine->cidr)->first();
+                if (is_null($prefixTest) === false) {
+
+                    $prefixTest->seen_at = Carbon::now();
+
+                    // If the last time the prefix was scraped is older than 7 days, update it
+                    if (strtotime($prefixTest->scraped_at) < Carbon::now()->subWeek()->timestamp) {
+                        $this->cli->br()->comment('===================================================');
+                        $this->cli->br()->comment('Updating older prefix whois info - ' . $prefixTest->ip . '/' . $prefixTest->cidr . ' [' . $ipAllocation->rir->name . ']')->br();
+
+                        $ipWhois = new Whois($prefixTest->ip);
+                        $parsedWhois = $ipWhois->parse();
+
+                        $prefixTest->name = $parsedWhois->name;
+                        $prefixTest->description = isset($parsedWhois->description[0]) ? $parsedWhois->description[0] : null;
+                        $prefixTest->description_full = json_encode($parsedWhois->description);
+                        $prefixTest->counrty_code = $parsedWhois->counrty_code;
+                        $prefixTest->owner_address = json_encode($parsedWhois->address);
+                        $prefixTest->raw_whois = $ipWhois->raw();
+                        $prefixTest->seen_at = Carbon::now();
+                        $prefixTest->scraped_at = Carbon::now();
+
+                        // Lets remove old emails for this prefix
+                        $prefixTest->emails()->delete();
+
+                        // Save new emails
+                        foreach ($parsedWhois->emails as $email) {
+                            $prefixEmail = new IPv4PrefixEmail;
+                            $prefixEmail->ipv4_prefix_id = $prefixTest->id;
+                            $prefixEmail->email_address = $email;
+
+                            // Check if its an abuse email
+                            if (in_array($email, $parsedWhois->abuse_emails)) {
+                                $prefixEmail->abuse_email = true;
+                            }
+
+                            $prefixEmail->save();
+                        }
+
+                        dump([
+                            'name' => $prefixTest->name,
+                            'description' => $prefixTest->description,
+                            'description_full' => json_decode($prefixTest->description_full, true),
+                            'counrty_code' => $prefixTest->counrty_code,
+                            'owner_address' => json_decode($prefixTest->owner_address, true),
+                            'abuse_emails' => $prefixTest->emails()->where('abuse_email', true)->get()->lists('email_address'),
+                            'emails' => $prefixTest->emails()->lists('email_address'),
+                        ]);
+
+                        unset($ipWhois);
+                        unset($parsedWhois);
+                    }
+
+                    // Update the prefix
+                    $prefixTest->save();
+                    continue;
+                }
+
+                $this->cli->br()->comment('===================================================');
+                $this->cli->br()->comment('Adding new prefix whois info - ' . $parsedLine->ip . '/' . $parsedLine->cidr . ' [' . $ipAllocation->rir->name . ']')->br();
+
+                $ipWhois = new Whois($parsedLine->ip);
+                $parsedWhois = $ipWhois->parse();
+
+                $ipv4Prefix = new IPv4Prefix;
+                $ipv4Prefix->rir_id = $ipAllocation->rir->id;
+                $ipv4Prefix->ip = $parsedLine->ip;
+                $ipv4Prefix->cidr = $parsedLine->cidr;
+                $ipv4Prefix->ip_dec_start = $this->ipUtils->ip2dec($parsedLine->ip);
+                $ipv4Prefix->ip_dec_end = ($this->ipUtils->ip2dec($parsedLine->ip) + $ipv4AmountCidrArray[$parsedLine->cidr]);
+                $ipv4Prefix->name = $parsedWhois->name;
+                $ipv4Prefix->description = isset($parsedWhois->description[0]) ? $parsedWhois->description[0] : null;
+                $ipv4Prefix->description_full = json_encode($parsedWhois->description);
+                $ipv4Prefix->counrty_code = $parsedWhois->counrty_code;
+                $ipv4Prefix->owner_address = json_encode($parsedWhois->address);
+                $ipv4Prefix->raw_whois = $ipWhois->raw();
+                $ipv4Prefix->seen_at = Carbon::now();
+                $ipv4Prefix->scraped_at = Carbon::now();
+                $ipv4Prefix->save();
+
+                // Save Prefix Emails
+                foreach ($parsedWhois->emails as $email) {
+                    $prefixEmail = new IPv4PrefixEmail;
+                    $prefixEmail->ipv4_prefix_id = $ipv4Prefix->id;
+                    $prefixEmail->email_address = $email;
+
+                    // Check if its an abuse email
+                    if (in_array($email, $parsedWhois->abuse_emails)) {
+                        $prefixEmail->abuse_email = true;
+                    }
+
+                    $prefixEmail->save();
+                }
+
+                dump([
+                    'name' => $ipv4Prefix->name,
+                    'description' => $ipv4Prefix->description,
+                    'description_full' => json_decode($ipv4Prefix->description_full, true),
+                    'counrty_code' => $ipv4Prefix->counrty_code,
+                    'owner_address' => json_decode($ipv4Prefix->owner_address, true),
+                    'abuse_emails' => $ipv4Prefix->emails()->where('abuse_email', true)->get()->lists('email_address'),
+                    'emails' => $ipv4Prefix->emails()->lists('email_address'),
+                ]);
+
+                // Remove all prefixes that are older than 1 day post udpating
+                IPv4Prefix::where('last_seen', '<', Carbon::yesterday())->delete();
             }
             fclose($fp);
+
+            //
         }
 
         $this->output->newLine(1);
