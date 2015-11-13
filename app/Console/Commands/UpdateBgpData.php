bc65f8d849b16a737d2a37f26b05af55779aec6a
Yif Swery <yiftachswr@gmail.com>
2015-11-13 16:22:50 +1300

    So we need to do the final clean at the end of the import not during

--- app/Console/Commands/UpdateBgpData.php
+++ app/Console/Commands/UpdateBgpData.php
@@ -189,13 +189,11 @@ class UpdateBgpData extends Command
                     'abuse_emails' => $ipv6Prefix->emails()->where('abuse_email', true)->get()->lists('email_address'),
                     'emails' => $ipv6Prefix->emails()->lists('email_address'),
                 ]);
-
-                // Remove all prefixes that are older than 1 day post udpating
-                IPv6Prefix::where('seen_at', '<', Carbon::yesterday())->delete();
             }
             fclose($fp);
 
-            //
+            // Remove all prefixes that are older than 1 day post udpating
+            IPv6Prefix::where('seen_at', '<', Carbon::yesterday())->delete();
         }
 
         $this->output->newLine(1);
