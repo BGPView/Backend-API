ec628f4799713d56e5e9a8aa1b55076358018d30
Yif Swery <yiftachswr@gmail.com>
2015-11-26 22:55:37 +1300

    Removing Progress bar as it takes a huge amount of blocking processing time

--- app/Console/Commands/UpdateAllocationLists.php
+++ app/Console/Commands/UpdateAllocationLists.php
@@ -17,8 +17,6 @@ class UpdateAllocationLists extends Command
     private $cli;
     private $ipUtils;
     private $bench;
-    private $progressStarted = false;
-    private $progressBar = null;
     /**
      * The name and signature of the console command.
      *
@@ -56,20 +54,16 @@ class UpdateAllocationLists extends Command
 
         foreach (Rir::all() as $rir) {
             $this->bench->start();
-            $this->progressStarted = false;
             $this->cli->br()->comment('===================================================');
             $this->cli->br()->comment('Downloading ' . $rir->name . ' allocation list');
 
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $rir->allocation_list_url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, [$this, 'downloadProgress']);
             curl_setopt($ch, CURLOPT_NOPROGRESS, false);
             curl_setopt($ch, CURLOPT_HEADER, 0);
             $output = curl_exec($ch);
             curl_close($ch);
-            $this->progressBar->finish();
-            $this->progressBar = null;
             $this->output->newLine(1);
             $this->updateDb($rir, $output);
             $this->output->newLine(2);
@@ -103,9 +97,7 @@ class UpdateAllocationLists extends Command
     {
         $lines = explode("\n", $list);
         $this->cli->br()->comment('Updating DB with the ' . $rir->name . ' allocation list ('.count($lines).' entries)');
-
-        $bar = $this->output->createProgressBar(count($lines));
-
+        
         $ipv4AmountCidrArray = $this->ipUtils->IPv4cidrIpCount($reverse = true);
         $ipv6AmountCidrArray = $this->ipUtils->IPv6cidrIpCount();
 
@@ -173,13 +165,7 @@ class UpdateAllocationLists extends Command
                         $this->cli->br()->comment("Entering new IPv4 range: " . $ipv6->ip . " [" . $rir->name . "]");
                     }
                 }
-
-                $bar->advance();
-                // Continue the rest
             }
         }
-
-        $bar->finish();
-
     }
 }
