1ec89fd63898f34e073b7a8b0eee1f64d88bc4ef
Yif Swery <yiftachswr@gmail.com>
2016-03-05 16:32:30 +1300

    Lets have the ability for RIR to be empty

---
+++ database/migrations/2016_03_04_090911_nullable_rir_id_asn.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NullableRirIdAsn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('asns', function($table)
+        {
+            $table->integer('rir_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('asns', function($table)
+        {
+            $table->integer('rir_id')->unsigned()->index()->change();
+        });
+
+    }
+}
