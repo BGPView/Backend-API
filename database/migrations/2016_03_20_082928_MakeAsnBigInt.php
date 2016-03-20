4e3e8f5f5225b22345ffcf72774913f3f2a310f1
Yif Swery <yiftachswr@gmail.com>
2016-03-20 21:47:45 +1300

    Lets make ASN bigInt since some ASNs (private ASNs) are large numbers

---
+++ database/migrations/2016_03_20_082928_MakeAsnBigInt.php
@@ -0,0 +1,117 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeAsnBigInt extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rir_asn_allocations', function($table)
+        {
+            $table->bigInteger('asn')->change();
+        });
+
+        Schema::table('asns', function($table)
+        {
+            $table->bigInteger('asn')->change();
+        });
+
+        Schema::table('ipv4_bgp_prefixes', function($table)
+        {
+            $table->bigInteger('asn')->change();
+        });
+
+        Schema::table('ipv6_bgp_prefixes', function($table)
+        {
+            $table->bigInteger('asn')->change();
+        });
+
+        Schema::table('ix_members', function($table)
+        {
+            $table->bigInteger('asn')->change();
+        });
+
+        Schema::table('ipv4_peers', function($table)
+        {
+            $table->bigInteger('asn_1')->change();
+            $table->bigInteger('asn_2')->change();
+        });
+
+        Schema::table('ipv6_peers', function($table)
+        {
+            $table->bigInteger('asn_1')->change();
+            $table->bigInteger('asn_2')->change();
+        });
+
+        Schema::table('ipv4_bgp_table', function($table)
+        {
+            $table->bigInteger('asn')->change();
+        });
+
+        Schema::table('ipv6_bgp_table', function($table)
+        {
+            $table->bigInteger('asn')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rir_asn_allocations', function($table)
+        {
+            $table->integer('asn')->change();
+        });
+
+        Schema::table('asns', function($table)
+        {
+            $table->integer('asn')->change();
+        });
+
+        Schema::table('ipv4_bgp_prefixes', function($table)
+        {
+            $table->integer('asn')->change();
+        });
+
+        Schema::table('ix_members', function($table)
+        {
+            $table->integer('asn')->change();
+        });
+
+        Schema::table('ipv6_bgp_prefixes', function($table)
+        {
+            $table->integer('asn')->change();
+        });
+
+        Schema::table('ipv4_peers', function($table)
+        {
+            $table->integer('asn_1')->change();
+            $table->integer('asn_2')->change();
+        });
+
+        Schema::table('ipv6_peers', function($table)
+        {
+            $table->integer('asn_1')->change();
+            $table->integer('asn_2')->change();
+        });
+
+        Schema::table('ipv4_bgp_table', function($table)
+        {
+            $table->integer('asn')->change();
+        });
+
+        Schema::table('ipv6_bgp_table', function($table)
+        {
+            $table->integer('asn')->change();
+        });
+    }
+}
