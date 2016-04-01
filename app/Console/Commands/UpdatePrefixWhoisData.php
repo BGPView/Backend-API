7f22448eb24d0594c28952c90bd2c0e92d431334
Yif Swery <yiftachswr@gmail.com>
2016-04-02 09:00:07 +1300

    limiting the number per round to update on the whois prefixes

--- app/Console/Commands/UpdatePrefixWhoisData.php
+++ app/Console/Commands/UpdatePrefixWhoisData.php
@@ -58,6 +58,8 @@ class UpdatePrefixWhoisData extends Command
     {
         $this->updatePrefixes(6);
         $this->updatePrefixes(4);
+        $this->updateOldPrefixes(6);
+        $this->updateOldPrefixes(4);
     }
 
     private function updatePrefixes($ipVersion)
@@ -212,8 +214,6 @@ class UpdatePrefixWhoisData extends Command
                 'description_full' => $newPrefixWhois->description_full,
                 'counrty_code' => $newPrefixWhois->counrty_code,
                 'owner_address' => $newPrefixWhois->owner_address,
-                'abuse_emails' => $newPrefixWhois->emails()->where('abuse_email', true)->get()->lists('email_address'),
-                'emails' => $newPrefixWhois->emails()->lists('email_address'),
             ]);
 
         }
@@ -227,5 +227,73 @@ class UpdatePrefixWhoisData extends Command
             $this->bench->getMemoryPeak()
         ))->br();
 
+    }
+
+    private function updateOldPrefixes($ipVersion)
+    {
+        $ipVersion = (string) $ipVersion;
+        $this->bench->start();
+        $this->cli->br()->comment('===================================================');
+        $threeWeeksAgo = Carbon::now()->subWeeks(3)->timestamp;
+
+        $this->cli->br()->comment('Getting all OLD IPv' . $ipVersion . 'prefixes from the whois table');
+
+        $className = 'App\Models\IPv' . $ipVersion . 'PrefixWhois';
+        $oldPrefixes = $className::where('updated_at', '<', Carbon::now()->subMonth())->orderBy('updated_at', 'ASC')->limit(2000)->get();
+
+        foreach ($oldPrefixes as $oldPrefix) {
+            $this->cli->br()->comment('===================================================');
+            $this->cli->br()->comment('Updating prefix whois info - ' . $oldPrefix->ip . '/' . $oldPrefix->cidr)->br();
+
+            $ipWhois = new Whois($oldPrefix->ip, $oldPrefix->cidr);
+            $parsedWhois = $ipWhois->parse();
+
+            // If null, lets skip
+            if (is_null($parsedWhois) === true) {
+                $this->cli->br()->error('Seems that whois server returned no results for prefix');
+                continue;
+            }
+
+            $oldPrefix->name = $parsedWhois->name;
+            $oldPrefix->description = isset($parsedWhois->description[0]) ? $parsedWhois->description[0] : null;
+            $oldPrefix->description_full = json_encode($parsedWhois->description);
+            $oldPrefix->counrty_code = $parsedWhois->counrty_code;
+            $oldPrefix->owner_address = json_encode($parsedWhois->address);
+            $oldPrefix->raw_whois = $ipWhois->raw();
+            $oldPrefix->save();
+
+            // Save Prefix Emails
+            $oldPrefix->emails()->delete();
+            foreach ($parsedWhois->emails as $email) {
+                $className = 'App\Models\IPv' . $ipVersion . 'PrefixWhoisEmail';
+                $prefixEmail = new $className;
+                $prefixEmail->prefix_whois_id = $oldPrefix->id;
+                $prefixEmail->email_address = $email;
+
+                // Check if its an abuse email
+                if (in_array($email, $parsedWhois->abuse_emails)) {
+                    $prefixEmail->abuse_email = true;
+                }
+
+                $prefixEmail->save();
+            }
+
+            dump([
+                'name' => $oldPrefix->name,
+                'description' => $oldPrefix->description,
+                'description_full' => $oldPrefix->description_full,
+                'counrty_code' => $oldPrefix->counrty_code,
+                'owner_address' => $oldPrefix->owner_address,
+            ]);
+
+        }
+
+        $this->output->newLine(1);
+        $this->bench->end();
+        $this->cli->info(sprintf(
+            'Time: %s, Memory: %s',
+            $this->bench->getTime(),
+            $this->bench->getMemoryPeak()
+        ))->br();
     }
 }
