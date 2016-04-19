cfea9dc636aac91108000ca8803def40e15433bd
Yif Swery <yiftachswr@gmail.com>
2016-04-19 22:28:21 +1200

    adding migration for the DNS ip_dec record

---
+++ database/migrations/2016_04_19_102145_add_ip_dec_dns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIpDecDns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dns_records', function($table)
+        {
+            $table->decimal('ip_dec', 39, 0)->unsigned()->index()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dns_records', function($table)
+        {
+            $table->dropColumn('ip_dec');
+        });
+    }
+}
