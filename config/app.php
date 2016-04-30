b5175333cd02ded3f201a90b71e92e16ef217eb5
Yif Swery <yiftachswr@gmail.com>
2016-04-30 14:43:00 +1200

    use the new mongo lib service provider

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
     ],
 
