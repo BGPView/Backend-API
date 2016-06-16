fb1ce3a3a99ca27b3ea7c433856e5c5c4eeb132f
Yif Swery <yiftachswr@gmail.com>
2016-06-16 19:14:03 +1200

    Adding base mapping for Elastic Search

--- app/Models/DNSRecord.php
+++ app/Models/DNSRecord.php
@@ -9,4 +9,43 @@ class DNSRecord {
 
     use ElasticquentTrait;
 
+    /**
+     * The elasticsearch settings.
+     *
+     * @var array
+     */
+    protected $indexSettings = [
+        'analysis' => [
+            'analyzer' => [
+                'string_lowercase' => [
+                    'tokenizer' => 'keyword',
+                    'filter' => [ 'asciifolding', 'lowercase', 'custom_replace' ],
+                ],
+            ],
+            'filter' => [
+                'custom_replace' => [
+                    'type' => 'pattern_replace',
+                    'pattern' => "[^a-z0-9 ]",
+                    'replacement' => "",
+                ],
+            ],
+        ],
+    ];
+
+    /**
+     * The elasticsearch mappings.
+     *
+     * @var array
+     */
+    protected $mappingProperties = [
+        'input' => [
+            'type' => 'string',
+            'analyzer' => 'string_lowercase'
+        ],
+    ];
+
+    public function getTable()
+    {
+        return 'dns_records';
+    }
 }
