1e6b30143c490c926b3c742bc85218bb27f373d9
Yif Swery <yiftachswr@gmail.com>
2016-06-16 22:41:38 +1200

    Make sure we have a status element

--- app/Console/Commands/UpdateAllocationLists.php
+++ app/Console/Commands/UpdateAllocationLists.php
@@ -156,7 +156,7 @@ class UpdateAllocationLists extends Command
             $data = explode('|', $line);
 
             // if it doesnt have 8 parts, skip
-            if (count($data) !== 8 && $data[6] != 'available' && $data[6] != 'reserved') {
+            if (count($data) !== 8 && isset($data[6]) === true && $data[6] != 'available' && $data[6] != 'reserved') {
                 continue;
             }
 
