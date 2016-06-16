913884c311e59424efa6bc654169487fbe39b874
Yif Swery <yiftachswr@gmail.com>
2016-06-16 19:14:22 +1200

    Indexing command set

--- app/Console/Commands/UpdateDNSTable.php
+++ app/Console/Commands/UpdateDNSTable.php
@@ -3,20 +3,20 @@
 namespace App\Console\Commands;
 
 use App\Helpers\IpUtils;
-use App\Jobs\ResolveDnsQuery;
 use App\Models\DNSRecord;
-use App\Services\Dns;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use League\CLImate\CLImate;
+use Illuminate\Support\Facades\Config;
 use Ubench;
 
 class UpdateDNSTable extends Command
 {
     use DispatchesJobs;
 
-    private $domainUrl;
+    protected $batchAmount = 50000;
+    protected $bench;
+    protected $ipUtils;
 
     /**
      * The name and signature of the console command.
@@ -35,10 +35,11 @@ class UpdateDNSTable extends Command
     /**
      * Create a new command instance.
      */
-    public function __construct()
+    public function __construct(Ubench $bench, IpUtils $ipUtils)
     {
         parent::__construct();
-        $this->domainUrl = 'https://wwws.io/api/full/539/'.config('wwws.email').'/'.config('wwws.password').'/';
+        $this->bench = $bench;
+        $this->ipUtils = $ipUtils;
     }
 
     /**
@@ -48,14 +49,133 @@ class UpdateDNSTable extends Command
      */
     public function handle()
     {
-        $filePath = 'us.txt';
+        $this->bench->start();
+        
+        // Setting a brand new index name
+        $entityIndexName = config('elasticquent.default_index') . '_dns';
+        $versionedIndex = $entityIndexName . '_' . time();
+        Config::set('elasticquent.default_index', $versionedIndex);
+
+        // create new index
+        DNSRecord::createIndex();
+
+        $currentCount = 0;
+        $filePath = 'final_output.csv';
+
+        $dnsModel = new DNSRecord;
+        $name = $dnsModel->getTable();
+        $params = ['body' => []];
+        $client = $dnsModel->getElasticSearchClient();
+
         $fp = fopen($filePath, 'r');
         if ($fp) {
             while (($line = fgets($fp)) !== false) {
-                $this->dispatch(new ResolveDnsQuery($line));
+                $line = trim($line);
+                if (empty($line) === true) {
+                    continue;
+                }
+
+                $parts = explode(',', $line, 3);
+
+                $data = [
+                    'input' => $parts[0],
+                    'type'  => $parts[1],
+                    'entry' => $parts[2],
+                ];
+
+                if ($parts[1] == 'A' || $parts[1] == 'AAAA') {
+                    $data['ip_dec'] = $this->ipUtils->ip2dec($data['entry']);
+                } else {
+                    $data['ip_dec'] = null;
+                }
+
+                $params['body'][] = [
+                    'index' => [
+                        '_index' => $versionedIndex,
+                        '_type' => $name,
+                    ]
+                ];
+                $params['body'][] = $data;
+                $currentCount++;
+
+                if ($currentCount > $this->batchAmount) {
+                    // Get our document body data.
+                    $client->bulk($params);
+                    $this->info('Inserted 50,000 records');
+
+                    // Reset the batching
+                    $currentCount = 0;
+                    $params['body'] = [];
+                }
             }
             fclose($fp);
+        }
+
+        // Insert the remaining entries
+        if (count($params['body']) > 0) {
+            $client->bulk($params);
+            $this->info('Inserted the remaining ' . count($params['body']) . ' records');
+        }
+
+        $this->hotSwapIndices($versionedIndex, $entityIndexName);
+
+        $this->output->newLine(1);
+        $this->bench->end();
+        $this->info(sprintf(
+            'Time: %s, Memory: %s',
+            $this->bench->getTime(),
+            $this->bench->getMemoryPeak()
+        ));
+
+
+
+
+    }
+
+    private function hotSwapIndices($versionedIndex, $entityIndexName)
+    {
+        $client = ClientBuilder::create()->build();
+
+        $indexExists       = $client->indices()->exists(['index' => $entityIndexName]);
+        $previousIndexName = null;
+        $indices           = $client->indices()->getAliases();
+
+        foreach ($indices as $indexName => $indexData) {
+            if (array_key_exists('aliases', $indexData) && isset($indexData['aliases'][$entityIndexName])) {
+                $previousIndexName = $indexName;
+                break;
+            }
+        }
+
+        if ($indexExists === true && $previousIndexName === null) {
+            $client->indices()->delete([
+                'index' => $entityIndexName,
+            ]);
+
+            $client->indices()->putAlias([
+                'name' => $entityIndexName,
+                'index' => $versionedIndex,
+            ]);
+        } else {
+            if ($previousIndexName !== null) {
+                $client->indices()->deleteAlias([
+                    'name' => $entityIndexName,
+                    'index' => $previousIndexName,
+                ]);
+            }
+            $client->indices()->putAlias([
+                'name' => $entityIndexName,
+                'index' => $versionedIndex,
+            ]);
+
+            if ($previousIndexName !== null) {
+                $client->indices()->delete([
+                    'index' => $previousIndexName,
+                ]);
+            }
         }
     }
 
 }
+
+
