122a3aa9b1404189cb9478a5db0dba4beee727f5
Yif Swery <yiftachswr@gmail.com>
2016-10-30 18:11:49 +1300

    Use RIR name instead of calling the related RIR model

--- app/Http/Controllers/ApiV1Controller.php
+++ app/Http/Controllers/ApiV1Controller.php
@@ -61,7 +61,7 @@ class ApiV1Controller extends ApiBaseController
         $output['traffic_ratio']        = $asnData->traffic_ratio;
         $output['owner_address']        = $asnData->owner_address;
 
-        $output['rir_allocation']['rir_name']           = empty($allocation->rir_id) !== true ? $allocation->rir->name : null;
+        $output['rir_allocation']['rir_name']           = empty($allocation->rir_name) !== true ? $allocation->rir_name : null;
         $output['rir_allocation']['country_code']       = isset($allocation->counrty_code) ? $allocation->counrty_code : null;
         $output['rir_allocation']['date_allocated']     = isset($allocation->date_allocated) ? $allocation->date_allocated . ' 00:00:00' : null;
         $output['rir_allocation']['allocation_status']  = isset($allocation->status) ? $allocation->status : 'unknown';
@@ -231,7 +231,7 @@ class ApiV1Controller extends ApiBaseController
         $output['country_codes']['rir_allocation_country_code'] = $allocation ? $allocation->counrty_code : null;
         $output['country_codes']['maxmind_country_code']        = $geoip ? $geoip->country->isoCode : null;
 
-        $output['rir_allocation']['rir_name']           = empty($allocation->rir_id) !== true ? $allocation->rir->name : null;
+        $output['rir_allocation']['rir_name']           = empty($allocation->rir_name) !== true ? $allocation->rir_name : null;
         $output['rir_allocation']['country_code']       = isset($allocation->counrty_code) ? $allocation->counrty_code : null;
         $output['rir_allocation']['ip']                 = isset($allocation->ip) ? $allocation->ip : null;
         $output['rir_allocation']['cidr']               = isset($allocation->cidr) ? $allocation->cidr : null;
@@ -336,7 +336,7 @@ class ApiV1Controller extends ApiBaseController
             return $b['cidr'] - $a['cidr'];
         });
 
-        $output['rir_allocation']['rir_name']           = isset($allocation->rir_id) && empty($allocation->rir_id) !== true ? $allocation->rir->name : null;
+        $output['rir_allocation']['rir_name']           = isset($allocation->rir_name) && empty($allocation->rir_name) !== true ? $allocation->rir_name : null;
         $output['rir_allocation']['country_code']       = isset($allocation->counrty_code) ? $allocation->counrty_code : null;
         $output['rir_allocation']['ip']                 = $rirIp;
         $output['rir_allocation']['cidr']               = $rirCidr;
