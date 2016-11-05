2ab7b355f489fa1250d11cc6ef14e947d4eb7470
Yif Swery <yiftachswr@gmail.com>
2016-11-05 14:28:27 +1300

    Add 4 more RIS points

--- scripts/update_bgp_ribs.sh
+++ scripts/update_bgp_ribs.sh
@@ -50,14 +50,14 @@ process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE
 
 process_rib http://data.ris.ripe.net/rrc00/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc01/latest-bview.gz
-# process_rib http://data.ris.ripe.net/rrc02/latest-bview.gz #Outdated and not used anymore
+# process_rib http://data.ris.ripe.net/rrc02/latest-bview.gz # Outdated and not used anymore
 process_rib http://data.ris.ripe.net/rrc03/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc04/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc05/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc06/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc07/latest-bview.gz
-# process_rib http://data.ris.ripe.net/rrc08/latest-bview.gz #Outdated and not used anymore
-# process_rib http://data.ris.ripe.net/rrc09/latest-bview.gz #Outdated and not used anymore
+# process_rib http://data.ris.ripe.net/rrc08/latest-bview.gz # Outdated and not used anymore
+# process_rib http://data.ris.ripe.net/rrc09/latest-bview.gz # Outdated and not used anymore
 process_rib http://data.ris.ripe.net/rrc10/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc11/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc12/latest-bview.gz
@@ -65,6 +65,10 @@ process_rib http://data.ris.ripe.net/rrc13/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc14/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc15/latest-bview.gz
 process_rib http://data.ris.ripe.net/rrc16/latest-bview.gz
+process_rib http://data.ris.ripe.net/rrc18/latest-bview.gz
+process_rib http://data.ris.ripe.net/rrc19/latest-bview.gz
+process_rib http://data.ris.ripe.net/rrc20/latest-bview.gz
+process_rib http://data.ris.ripe.net/rrc21/latest-bview.gz
 
 ###############################################################################################
 
