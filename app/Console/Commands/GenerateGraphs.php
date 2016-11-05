b7cd6e746b5d05a801e150f1585608857df8852f
Yif Swery <yiftachswr@gmail.com>
2016-11-06 08:59:16 +1300

    Add slashes

--- app/Console/Commands/GenerateGraphs.php
+++ app/Console/Commands/GenerateGraphs.php
@@ -143,7 +143,7 @@ class GenerateGraphs extends Command
                 $asnMeta = $asnsData->where('asn', (int) $relation['asn1'])->first();
                 if (is_null($asnMeta) !== true) {
                     $countryCode = empty($asnMeta->counrty_code) !== true ? ' [' . $asnMeta->counrty_code . ']' : '';
-                    $outputGraphvizText .= 'AS' . $relation['asn2'] . ' [tooltip="AS' . $asnMeta->asn . ' ~ ' . $asnMeta->description . $countryCode . '" URL="https://bgpview.io/asn/' . $asnMeta->asn . '"]';
+                    $outputGraphvizText .= 'AS' . $relation['asn2'] . ' [tooltip="AS' . $asnMeta->asn . ' ~ ' . addslashes($asnMeta->description) . $countryCode . '" URL="https://bgpview.io/asn/' . $asnMeta->asn . '"]';
                     $processedAsn[$relation['asn1']] = true;
                 }
             }
@@ -151,7 +151,7 @@ class GenerateGraphs extends Command
                 $asnMeta = $asnsData->where('asn', (int) $relation['asn2'])->first();
                 if (is_null($asnMeta) !== true) {
                     $countryCode = empty($asnMeta->counrty_code) !== true ? ' [' . $asnMeta->counrty_code . ']' : '';
-                    $outputGraphvizText .= 'AS' . $relation['asn2'] . ' [tooltip="AS' . $asnMeta->asn . ' ~ ' . $asnMeta->description . $countryCode . '" URL="https://bgpview.io/asn/' . $asnMeta->asn . '"]';
+                    $outputGraphvizText .= 'AS' . $relation['asn2'] . ' [tooltip="AS' . $asnMeta->asn . ' ~ ' . addslashes($asnMeta->description) . $countryCode . '" URL="https://bgpview.io/asn/' . $asnMeta->asn . '"]';
                     $processedAsn[$relation['asn2']] = true;
                 }
             }
