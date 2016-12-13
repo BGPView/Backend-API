24f7fc3d1f19cebda7a09c87d90aa4f875b0db6d
Yif Swery <yiftachswr@gmail.com>
2016-12-13 20:50:49 +1300

    Renaming key names for consistency

--- app/Http/Controllers/ApiV1Controller.php
+++ app/Http/Controllers/ApiV1Controller.php
@@ -61,7 +61,7 @@ class ApiV1Controller extends ApiBaseController
         $output['rir_allocation']['date_allocated']    = isset($allocation->date_allocated) ? $allocation->date_allocated . ' 00:00:00' : null;
         $output['rir_allocation']['allocation_status'] = isset($allocation->status) ? $allocation->status : null;
 
-        $output['iana_assignment']['status']        = $ianaAssignment->status;
+        $output['iana_assignment']['assignment_status']        = $ianaAssignment->status;
         $output['iana_assignment']['description']   = $ianaAssignment->description;
         $output['iana_assignment']['whois_server']  = $ianaAssignment->whois_server;
         $output['iana_assignment']['date_assigned'] = $ianaAssignment->date_assigned;
