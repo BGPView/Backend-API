8ca95044c7aeab5fa82765b1a341d43f3c5646b7
Yif Swery <yiftachswr@gmail.com>
2016-12-19 08:53:18 +1300

    Create the new Job handler for the ASN graphs

---
+++ app/Jobs/GenerateAsnGraphs.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\ASN;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use League\CLImate\CLImate;
+
+class GenerateAsnGraphs extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    protected $input_asn;
+    protected $ip_version;
+    protected $upstreams;
+    protected $cli;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($inputAsn, $ipVersion, $upstreams)
+    {
+        $this->input_asn  = $inputAsn;
+        $this->ip_version = $ipVersion;
+        $this->upstreams  = $upstreams;
+        $this->cli        = new CLImate();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $inputAsn  = $this->input_asn;
+        $ipVersion = $this->ip_version;
+        $upstreams = $this->upstreams;
+
+        $relations = [];
+        foreach ($upstreams as $upstream) {
+            foreach ($upstream['bgp_paths'] as $path) {
+                $asns = explode(' ', $path);
+
+                foreach ($asns as $key => $asn) {
+                    $asnList[] = $asn;
+
+                    // If its the last ASN, then stop
+                    if (isset($asns[$key + 1]) !== true) {
+                        continue;
+                    }
+
+                    if (isset($relations[$asn . '-' . $asns[$key + 1]]) === true) {
+                        $relations[$asn . '-' . $asns[$key + 1]]['weight']++;
+                        continue;
+                    }
+
+                    $relations[$asn . '-' . $asns[$key + 1]] = [
+                        'weight' => 1,
+                        'asn1'   => $asn,
+                        'asn2'   => $asns[$key + 1],
+                    ];
+                }
+            }
+        }
+
+        $keyAsns      = [];
+        $realRelation = [];
+        foreach ($relations as $relation) {
+            $keyAsns[$relation['asn1']][] = $relation;
+        }
+
+        $asnsData = ASN::whereIn('asn', array_unique($asnList))->get();
+
+        foreach ($keyAsns as $groupedAsns) {
+            $highestNumber = $groupedAsns[0]['weight'];
+            foreach ($groupedAsns as $asn) {
+                if ($asn['weight'] > $highestNumber) {
+                    $highestNumber = $asn['weight'];
+                }
+            }
+
+            // Set the lowest to be 1;
+            if ($highestNumber > $this->maxLineThickness) {
+                $devider = $highestNumber / $this->maxLineThickness;
+            } else {
+                $devider = 1;
+            }
+            foreach ($groupedAsns as $asn) {
+                $asn['weight']  = $asn['weight'] / $devider;
+                $realRelation[] = $asn;
+            }
+        }
+
+        $outputGraphvizText = 'digraph "AS' . $inputAsn . ' ' . $ipVersion . ' Upstream Graph" {' . PHP_EOL;
+        $outputGraphvizText .= 'rankdir=LR;' . PHP_EOL;
+        $outputGraphvizText .= 'node [style=filled,fillcolor="#ffffff",fontcolor="#2C94B3"];' . PHP_EOL;
+
+        $processedAsn = [];
+        foreach ($realRelation as $relation) {
+
+            // Add labels and hyperlinks
+            if (isset($processedAsn[$relation['asn1']]) !== true) {
+                $asnMeta = $asnsData->where('asn', (int) $relation['asn1'])->first();
+                if (is_null($asnMeta) !== true) {
+                    $countryCode = empty($asnMeta->counrty_code) !== true ? ' [' . $asnMeta->counrty_code . ']' : '';
+                    $description = strlen($asnMeta->description) > 35 ? $asnMeta->name : $asnMeta->description;
+                    $description = str_replace("'", "", $description);
+                    $outputGraphvizText .= 'AS' . $relation['asn1'] . ' ';
+                    $outputGraphvizText .= '[';
+                    $outputGraphvizText .= 'tooltip="AS' . $asnMeta->asn . ' ~ ' . addslashes($description) . $countryCode . '" ';
+                    $outputGraphvizText .= 'URL="https://bgpview.io/asn/' . $asnMeta->asn . '" ';
+                    $outputGraphvizText .= 'fontcolor="#2C94B3" ';
+                    $outputGraphvizText .= ']' . PHP_EOL;
+                    $processedAsn[$relation['asn1']] = true;
+                }
+            }
+            if (isset($processedAsn[$relation['asn2']]) !== true) {
+                $asnMeta = $asnsData->where('asn', (int) $relation['asn2'])->first();
+                if (is_null($asnMeta) !== true) {
+                    $countryCode = empty($asnMeta->counrty_code) !== true ? ' [' . $asnMeta->counrty_code . ']' : '';
+                    $description = strlen($asnMeta->description) > 35 ? $asnMeta->name : $asnMeta->description;
+                    $description = str_replace("'", "", $description);
+                    $outputGraphvizText .= 'AS' . $relation['asn2'] . ' ';
+                    $outputGraphvizText .= '[';
+                    $outputGraphvizText .= 'tooltip="AS' . $asnMeta->asn . ' ~ ' . addslashes($description) . $countryCode . '" ';
+                    $outputGraphvizText .= 'URL="https://bgpview.io/asn/' . $asnMeta->asn . '" ';
+                    $outputGraphvizText .= ']' . PHP_EOL;
+                    $processedAsn[$relation['asn2']] = true;
+                }
+            }
+
+            $outputGraphvizText .= 'AS' . $relation['asn1'] . ' -> AS' . $relation['asn2'] . ' [ penwidth = ' . $relation['weight'] . ' ];' . PHP_EOL;
+        }
+        $outputGraphvizText .= 'AS' . $inputAsn . '[fontcolor="#880000"]' . PHP_EOL;
+        $outputGraphvizText .= '}' . PHP_EOL;
+
+        exec('echo \'' . $outputGraphvizText . '\' | dot -Tsvg -o ' . public_path() . '/assets/graphs/AS' . $inputAsn . '_' . $ipVersion . '.svg');
+
+        $this->cli->br()->comment('Generated graph for AS' . $this->input_asn . ' [' . $this->ip_version . ']');
+    }
+}
