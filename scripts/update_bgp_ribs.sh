109c1f5e9b4c1166eb1ffd04a7abc6710f42f407
Yif Swery <yiftachswr@gmail.com>
2018-07-20 04:18:22 +0000

    Upgrade BGPDUMP from 1.4.99.15 to 1.5

--- scripts/update_bgp_ribs.sh
+++ scripts/update_bgp_ribs.sh
@@ -30,7 +30,7 @@ then
 	RIB_FOLDER=`curl ftp://$BASE_URL/ | tail -2 | head -1 | awk '{print $(NF)}'`
 	RIB_FILE=`curl ftp://$BASE_URL/$RIB_FOLDER/RIBS/ | tail -1 | awk '{print $(NF)}'`
 fi
-process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" &
+process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" 
 
 # IPv6 RouteViews
 BASE_URL="archive.routeviews.org/route-views6/bgpdata";
@@ -41,42 +41,35 @@ then
         RIB_FOLDER=`curl ftp://$BASE_URL/ | tail -2 | head -1 | awk '{print $(NF)}'`
         RIB_FILE=`curl ftp://$BASE_URL/$RIB_FOLDER/RIBS/ | tail -1 | awk '{print $(NF)}'`
 fi
-process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" &
+process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" 
 
 
 
 ###############################################################################################
 
-process_rib http://data.ris.ripe.net/rrc00/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc01/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc00/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc01/latest-bview.gz "axel -o" 
 # process_rib http://data.ris.ripe.net/rrc02/latest-bview.gz # Outdated and not used anymore
-process_rib http://data.ris.ripe.net/rrc03/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc04/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc05/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc06/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc07/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc03/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc04/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc05/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc06/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc07/latest-bview.gz "axel -o" 
 # process_rib http://data.ris.ripe.net/rrc08/latest-bview.gz # Outdated and not used anymore
 # process_rib http://data.ris.ripe.net/rrc09/latest-bview.gz # Outdated and not used anymore
-process_rib http://data.ris.ripe.net/rrc10/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc11/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc12/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc13/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc14/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc15/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc16/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc18/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc19/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc20/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc21/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc22/latest-bview.gz "axel -o" &
-process_rib http://data.ris.ripe.net/rrc23/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc10/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc11/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc12/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc13/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc14/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc15/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc16/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc18/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc19/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc20/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc21/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc22/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc23/latest-bview.gz "axel -o" 
 ###############################################################################################
-
-sleep 5
-
-while pgrep -x "wget|axel|bgpdump" > /dev/null;
-do
-    sleep 3
-done
 
 exit 0
