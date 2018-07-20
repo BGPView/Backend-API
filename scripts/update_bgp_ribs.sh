e20ad3468f81839e83a3d815a5ab6789378b7976
Yif Swery <yiftachswr@gmail.com>
2018-07-20 16:39:59 +1200

    Use a different way to concatenate the BGP feeds

--- scripts/update_bgp_ribs.sh
+++ scripts/update_bgp_ribs.sh
@@ -14,7 +14,7 @@ function process_rib {
         filename=$1
         extension="${filename##*.}"
         $2 ./storage/temp_rib_$random_name.$extension $1;
-        ./scripts/bgpdump -m ./storage/temp_rib_$random_name.$extension >> ./storage/bgp_lines.txt;
+        ./scripts/bgpdump -m ./storage/temp_rib_$random_name.$extension >> ./storage/$random_name_bgp_out.txt
         rm -f ./storage/temp_rib_$random_name.$extension;
     fi
 }
@@ -30,7 +30,7 @@ then
 	RIB_FOLDER=`curl ftp://$BASE_URL/ | tail -2 | head -1 | awk '{print $(NF)}'`
 	RIB_FILE=`curl ftp://$BASE_URL/$RIB_FOLDER/RIBS/ | tail -1 | awk '{print $(NF)}'`
 fi
-process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" 
+process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" &
 
 # IPv6 RouteViews
 BASE_URL="archive.routeviews.org/route-views6/bgpdata";
@@ -41,35 +41,47 @@ then
         RIB_FOLDER=`curl ftp://$BASE_URL/ | tail -2 | head -1 | awk '{print $(NF)}'`
         RIB_FILE=`curl ftp://$BASE_URL/$RIB_FOLDER/RIBS/ | tail -1 | awk '{print $(NF)}'`
 fi
-process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" 
+process_rib $BASE_URL/$RIB_FOLDER/RIBS/$RIB_FILE "wget -6 -O" &
 
 
 
 ###############################################################################################
 
-process_rib http://data.ris.ripe.net/rrc00/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc01/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc00/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc01/latest-bview.gz "axel -o" &
 # process_rib http://data.ris.ripe.net/rrc02/latest-bview.gz # Outdated and not used anymore
-process_rib http://data.ris.ripe.net/rrc03/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc04/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc05/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc06/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc07/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc03/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc04/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc05/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc06/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc07/latest-bview.gz "axel -o" &
 # process_rib http://data.ris.ripe.net/rrc08/latest-bview.gz # Outdated and not used anymore
 # process_rib http://data.ris.ripe.net/rrc09/latest-bview.gz # Outdated and not used anymore
-process_rib http://data.ris.ripe.net/rrc10/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc11/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc12/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc13/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc14/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc15/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc16/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc18/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc19/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc20/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc21/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc22/latest-bview.gz "axel -o" 
-process_rib http://data.ris.ripe.net/rrc23/latest-bview.gz "axel -o" 
+process_rib http://data.ris.ripe.net/rrc10/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc11/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc12/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc13/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc14/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc15/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc16/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc18/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc19/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc20/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc21/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc22/latest-bview.gz "axel -o" &
+process_rib http://data.ris.ripe.net/rrc23/latest-bview.gz "axel -o" &
 ###############################################################################################
+
+sleep 5
+
+while pgrep -x "wget|axel|bgpdump" > /dev/null;
+do
+    sleep 3
+done
+
+# Combine all output RIB files into the single file
+echo "Combining all the output BGP files into one..."
+cat ./storage/*_bgp_out.txt > ./storage/bgp_lines.txt
+rm -f ./storage/*_bgp_out.txt
 
 exit 0
